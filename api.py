d89fb69cb3ee6193ad1e5013c0e0e1fe9bd819b9
WILTONpedro <wiltonpedro35@gmail.com>
2025-09-20 03:05:08 -0300

    Primeiro commit - API Flask

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import pickle
+import os
 
 app = Flask(__name__)
 
@@ -19,5 +20,5 @@ def classificar():
     
     return jsonify({"vaga": vaga})
 
-if __name__ == "__main__":
-    app.run(port=5000)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
