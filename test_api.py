c566d83362b93ebd71ff76fc7a535df3854b94cb
WILTONpedro <wiltonpedro35@gmail.com>
2025-10-05 16:28:56 -0300

    Delete test_api.py

--- test_api.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "http://localhost:5000/classificar"
-data = {"texto": "Experiência com vendas e atendimento ao cliente."}
-
-response = requests.post(url, json=data)  # note o `json=data` aqui
-print(response.json())
