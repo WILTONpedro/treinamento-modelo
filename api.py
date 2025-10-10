30bc42b12ef74c1a7f265e14ed7fdf621eefa152
WILTONpedro <wiltonpedro35@gmail.com>
2025-10-10 15:17:47 -0300

    Update api.py

--- api.py
+++ api.py
@@ -193,4 +193,4 @@ def healthcheck():
 
 if __name__ == "__main__":
     print("🚀 Servidor Flask iniciado em http://127.0.0.1:5000")
-    app.run(debug=True, host="0.0.0.0", port=5000)}
+    app.run(debug=True, host="0.0.0.0", port=5000)
