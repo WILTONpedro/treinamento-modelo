61704c18485a01f10e215bf37432c2b3d63ce397
WILTONpedro <wiltonpedro35@gmail.com>
2025-11-26 15:39:23 -0300

    Update model version in api.py

--- api.py
+++ api.py
@@ -157,7 +157,7 @@ def analisar_com_gemini(texto_curriculo):
     """
 
     try:
-        model = genai.GenerativeModel('gemini-1.5-flash-latest')
+        model = genai.GenerativeModel('gemini-1.5-flash')
         response = model.generate_content(prompt)
         clean_json = response.text.replace("```json", "").replace("```", "").strip()
         return json.loads(clean_json)
